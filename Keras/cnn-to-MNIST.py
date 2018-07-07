60b59f449f4b9513f5429e52d413dddefb137c05
Marani Matias Ezequiel <maranimatias@gmail.com>
2018-07-07 18:55:08 -0300

    Keras1 to keras2

--- Keras/cnn-to-MNIST.py
+++ Keras/cnn-to-MNIST.py
@@ -9,8 +9,8 @@ import numpy as np
 train_imgs = train_imgs.astype(np.float32)/255.
 test_imgs = test_imgs.astype(np.float32)/255.
 
-aet = 40000
-aev = 40000 * 10000
+aet = 60000
+aev = 60000 * 10000
 
 nrows = 28
 ncols = 28
@@ -26,11 +26,11 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Reshape
 
 # upsample is reverse of pooling
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
 import sklearn.metrics as metritcs
 
 """
-#============================================================================
+# ============================================================================
 # simple model
 model = Sequential()
 # 128 vector de caracteristicas
@@ -43,33 +43,26 @@ model.compile(loss="binary_crossentropy", optimizer="adadelta")
 
 history = model.fit(train_imgs.reshape([-1, 784])[:aet],
                     train_imgs.reshape([-1, 784])[:aet],
-                    nb_epoch=100,
+                    epochs=100,
                     validation_data=(train_imgs.reshape([-1, 784])[aet:aev],
                                      train_imgs.reshape([-1, 784])[aet:aev])
                     )
 
 plt.matshow(model.predict(train_imgs[2].reshape(
     [1, 784])).reshape([28, 28]), cmap=plt.cm.gray)
-#============================================================================
+# ============================================================================
 """
 
 # convolutions
 model = Sequential()
 
-model.add(Convolution2D(24, 5, 5, input_shape=(1, nrows, ncols,), border_mode='same'))
-model.add(Activation('relu'))
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Convolution2D(24, 5, 5, border_mode='same'))
-model.add(Activation('relu'))
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Convolution2D(24, 5, 5, border_mode='same'))
-model.add(Activation('relu'))
-
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(24, (5, 5), input_shape=(1, nrows, ncols,),
+                 padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='valid'))
+model.add(Conv2D(24, (5, 5), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='valid'))
+model.add(Conv2D(24, (5, 5), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), padding='valid'))
 
 model.add(Flatten())
 
@@ -85,7 +78,7 @@ model.add(Activation('relu'))
 
 model.add(Reshape(24, 3, 3))
 
-model.add(Convolution2D(24, 5, 5, border_mode='same'))
+model.add(Conv2D(24, (5, 5), padding='same'))
 model.add(Activation('relu'))
 
 model.add(UpSampling2D(size=(2, 2)))
@@ -94,19 +87,18 @@ model.add(ZeroPadding2D(padding=(0, 1, 0, 1)))
 model.summary()
 
 model.add(Reshape(24, 3, 3))
-model.add(Convolution2D(24, 5, 5, border_mode='same'))
+model.add(Conv2D(24, (5, 5), padding='same'))
 model.add(Activation('relu'))
 model.add(UpSampling2D(size=(2, 2)))
 
 # reduce to 1 channel
-model.add(Convolution2D(1, 5, 5, border_mode='same'))
-model.add(Activation('sigmoid'))
+model.add(Conv2D(1, (5, 5), padding='same', activation='sigmoid'))
 
 model.compile(loss='binary_crossestropy', optimizer='adadelta')
 
 history = model.fit(train_imgs.reshape([-1, 1, 28, 28])[:aet],
                     train_imgs.reshape([-1, 1, 28, 284])[:aet],
-                    nb_epoch=100,
+                    epochs=100,
                     validation_data=(train_imgs.reshape([-1, 1, 28, 28])[aet:aev],
                                      train_imgs.reshape([-1, 1, 28, 28])[aet:aev])
                     )
